b5f68cfc95d690d89dabe066f7ad62ed289104f6
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-03-11 18:17:29 +0100

    5 max bullets

--- AwesomeGame/Main.cpp
+++ AwesomeGame/Main.cpp
@@ -6,6 +6,55 @@
 #pragma comment (lib, "SDL/libx86/SDL2main.lib")
 #pragma comment (lib, "SDL_Mixer/libx86/SDL2_mixer.lib")
 
+#define WINDOW_WIDTH 1100
+#define WINDOW_HEIGHT 700
+#define MAX_ACTIVE_BULLETS 5
+
+
+struct PlayerInput {
+	bool pressing_W = false;
+	bool pressing_A = false;
+	bool pressing_S = false;
+	bool pressing_D = false;
+}; 
+
+struct Bullet {
+
+
+	int speed;
+	SDL_Rect bullet;
+	Bullet() {
+		bullet.x = 0;
+		bullet.y = 0;
+		bullet.w = 30;
+		bullet.h = 20;
+		speed = 1;
+	}
+	Bullet(int x, int y) {
+		bullet.x = x;
+		bullet.y = y;
+		bullet.w = 30;
+		bullet.y = 20;
+		speed = 0;
+	}
+};
+
+Bullet *active_bullets[MAX_ACTIVE_BULLETS];
+
+
+
+void Input();
+void MoveBullets(SDL_Renderer* renderer);
+
+
+
+SDL_Event event;
+
+SDL_Rect square{ 100,100,100,100 };
+
+PlayerInput player_input;
+bool loop = true;
+
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -16,89 +65,17 @@ int main(int argc, char* argv[]) {
 	Mix_Chunk *fx_sound = nullptr;
 	fx_sound = Mix_LoadWAV("fx/fx.wav");
 
-	bool loop = true;
-	bool bullet_on = false;
-
 	SDL_Window* window = SDL_CreateWindow("Awesome Game", SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+		SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_Event event;
-
-	SDL_Rect square;
-	square.x = 100;
-	square.y = 100;
-	square.w = 100;
-	square.h = 100;
-
-	struct Bullet {
-
-		int speed = 2;
-		SDL_Rect bullet;
-
-	};
-	
-	Bullet bullet;
-
-	bullet.bullet.w = 50;
-	bullet.bullet.h = 35;
 
 	while (loop) {
 
-		if (SDL_PollEvent(&event) != 0) {
+		Input();
+		MoveBullets(renderer);
 
-			if (event.type == SDL_QUIT)
-				loop = false;
-	
-			if (event.type == SDL_KEYDOWN) {
-				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
-					loop = false;
-				if (event.key.keysym.scancode == SDL_SCANCODE_UP){
-					if (square.y > 0) {
-						square.y -= 10;
-					}
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
-					if (square.x > 0) {
-						square.x -= 10;
-					}
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-					if (square.y < 600 - square.h) {
-						square.y += 10;
-					}
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
-					if (square.x < 800 - square.w) {
-						square.x += 10;
-					}
-				}
-				if (event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
-					Mix_PlayChannel(1,fx_sound,1);
-					bullet_on = true;
-					bullet.bullet.x = square.x + square.w - 10;
-					bullet.bullet.y = square.y + (square.h / 2) - 15;
-
-				}
-			}
-		}
-
-		if (bullet_on) {
-			if (bullet.bullet.x >= 800) {
-				bullet_on = false;
-			}
-			SDL_SetRenderDrawColor(renderer, 127, 255, 0, 255);
-			SDL_RenderFillRect(renderer, &bullet.bullet);
-
-			bullet.bullet.x += 1;
-
-		}
-
-		
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderFillRect(renderer, &square);
-
-
 		
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderPresent(renderer);
@@ -107,7 +84,103 @@ int main(int argc, char* argv[]) {
 		SDL_RenderClear(renderer);
 	}
 
-
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
+}
+
+void Input()
+{
+	if (SDL_PollEvent(&event) != 0) {
+		switch (event.type) {
+		case SDL_QUIT:
+			loop = false;
+			break;
+		case SDL_KEYDOWN: {
+			switch (event.key.keysym.scancode) {
+			case SDL_SCANCODE_A:
+				player_input.pressing_A = true;
+				break;
+			case SDL_SCANCODE_S:
+				player_input.pressing_S = true;
+				break;
+			case SDL_SCANCODE_D:
+				player_input.pressing_D = true;
+				break;
+			case SDL_SCANCODE_W:
+				player_input.pressing_W = true;
+				break;
+			case SDL_SCANCODE_SPACE:
+				for (int i = 0; i < MAX_ACTIVE_BULLETS; ++i) {
+					if (active_bullets[i] == nullptr) {
+						active_bullets[i] = new Bullet();
+						active_bullets[i]->bullet.x = square.x + square.w;
+						active_bullets[i]->bullet.y = square.y + square.h / 2 - active_bullets[i]->bullet.h / 2;
+						break;
+					}
+				}
+				break;
+			case SDL_SCANCODE_ESCAPE:
+				loop = false;
+				break;
+			default:
+				break;
+			}
+			break; }
+		case SDL_KEYUP: {
+			switch (event.key.keysym.scancode) {
+			case SDL_SCANCODE_A:
+				player_input.pressing_A = false;
+				break;
+			case SDL_SCANCODE_S:
+				player_input.pressing_S = false;
+				break;
+			case SDL_SCANCODE_D:
+				player_input.pressing_D = false;
+				break;
+			case SDL_SCANCODE_W:
+				player_input.pressing_W = false;
+				break;
+			default:
+				break;
+			}
+			break; }
+		default:
+			break;
+		}
+	}
+
+	if (player_input.pressing_A) {
+		square.x -= 1;
+	}
+	if (player_input.pressing_D) {
+		square.x += 1;
+	}
+	if (player_input.pressing_W) {
+		square.y -= 1;
+	}
+	if (player_input.pressing_S) {
+		square.y += 1;
+	}
+}
+
+void MoveBullets(SDL_Renderer* renderer)
+{
+	for (int i = 0; i < MAX_ACTIVE_BULLETS; ++i) {
+
+		Bullet* b = active_bullets[i];
+
+		if (b == nullptr)
+			continue;
+
+		if (b->bullet.x >= WINDOW_WIDTH) {
+			delete b;
+			active_bullets[i] = nullptr;
+		}
+		else {
+			b->bullet.x += b->speed;
+			SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+			SDL_RenderFillRect(renderer, &b->bullet);
+		}
+	}
 }
